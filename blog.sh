c696ecac20ebb4be55c173011a0c1c538c415bb3
Siva Mahadevan <me@svmhdvn.name>
2024-06-21 18:50:47 -0400

    shellcheck cleanup

--- blog.sh
+++ blog.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-
-set -e
+set -eu
 
 USERNAME=svmhdvn
 FULLNAME="Siva Mahadevan"
@@ -27,8 +26,9 @@ iso8601_date_only() {
 gmi_feed_entries() {
     tsvdb="$1"
     category="$2"
-    grep "${category}/" "$tsvdb" | while IFS="$tabchar" read -r created updated title f; do
-        printf '=> /%s %s - %s\n' "$f" "$(echo "$created" | iso8601_date_only)" "$title"
+    grep "${category}/" "${tsvdb}" | while IFS="${tabchar}" read -r created updated title f; do
+        created_date="$(echo "${created}" | iso8601_date_only)"
+        echo "=> /${f} ${created_date} - ${title}"
     done
 }
 
@@ -40,26 +40,28 @@ gmi_title() {
 # front page content)
 index_tsv() {
     find published -type f -name '*.gmi' ! -name 'index.gmi' | while IFS= read -r gmi; do
-        git_history=$(git_timestamps_iso8601 "$gmi")
-        created="$(echo "$git_history" | tail -1)"
-        updated="$(echo "$git_history" | head -1)"
+        git_history=$(git_timestamps_iso8601 "${gmi}")
+        created="$(echo "${git_history}" | tail -1)"
+        updated="$(echo "${git_history}" | head -1)"
+        gmi_title="$(sed -n '/^# /{s/# //p; q}' "${gmi}")"
         printf '%s\t%s\t%s\t%s\n' \
-            "$created" \
-            "$updated" \
-            "$(sed -n '/^# /{s/# //p; q}' "$gmi")" \
+            "${created}" \
+            "${updated}" \
+            "${gmi_title}" \
             "${gmi#published/}"
     done | sort -r
 }
 
+# $1 = article
+# $2 = nav_categories
 article_to_html() {
-    article="$1"
-    nav_categories="$2"
-
-    built_gmi="${article%.article}.gmi"
-    title="$(gmi_title < "$built_gmi")"
+    built_gmi="${1%.article}.gmi"
+    title="$(gmi_title < "${built_gmi}")"
     published_gmi="published/${built_gmi#build/}"
 
-    last_updated=$( (git_timestamps_human "$published_gmi" || git_timestamps_human published) | head -1)
+    # shellcheck disable=SC2310
+    last_updated_history="$(git_timestamps_human "${published_gmi}" || git_timestamps_human published)"
+    last_updated="$(echo "${last_updated_history}" | head -1)"
 
     cat <<EOF
 <!DOCTYPE html>
@@ -76,12 +78,14 @@ article_to_html() {
     <header>
       <nav>
         <strong><a href='/'>&lt;~${USERNAME}&gt;</a></strong>
-        <ul>${nav_categories}</ul>
+        <ul>$2</ul>
       </nav>
       <h1>${title}</h1>
     </header>
     <main>
-$(cat "$article")
+EOF
+    cat "$1"
+    cat <<EOF
     </main>
     <footer>
       <p>Last edited on ${last_updated}.</p>
@@ -95,7 +99,9 @@ EOF
 
 }
 
+# $1 = tsvdb
 generate_atom_feed() {
+    last_updated="$(git_timestamps_iso8601 . | head -1)"
     cat <<EOF
 <?xml version="1.0" encoding="utf-8"?>
 <feed xmlns="http://www.w3.org/2005/Atom">
@@ -103,7 +109,7 @@ generate_atom_feed() {
   <id>tag:${DOMAIN},2024-02-01:default-atom-feed</id>
   <link href="https://${DOMAIN}/" />
   <link href="https://${DOMAIN}/feed.xml" rel="self" type="application/atom+xml" />
-  <updated>$(git_timestamps_iso8601 . | head -1)</updated>
+  <updated>${last_updated}</updated>
   <generator uri="https://git.sr.ht/~${USERNAME}/blog/tree/main/item/blog.sh">blog.sh</generator>
   <icon>https://${DOMAIN}/favicon.ico</icon>
   <logo>https://${DOMAIN}/siva.jpg</logo>
@@ -115,29 +121,30 @@ generate_atom_feed() {
   </author>
 EOF
 
-    tsvdb="$1"
-    while IFS="$tabchar" read -r created updated title gmi; do
-        day=$(echo "$created" | iso8601_date_only)
+    while IFS="${tabchar}" read -r created updated title gmi; do
+        day=$(echo "${created}" | iso8601_date_only)
+        escaped_html="$(escape_html < "build/${gmi%.gmi}.article")"
         cat <<EOF
 <entry>
   <title>${title}</title>
   <id>tag:${DOMAIN},${day}:${gmi%.gmi}.html</id>
   <link rel="alternate" href="https://${DOMAIN}/${gmi%.gmi}.html"/>
-  <published>$created</published>
-  <updated>$updated</updated>
+  <published>${created}</published>
+  <updated>${updated}</updated>
   <content type="html">
-$(escape_html < "build/${gmi%.gmi}.article")
+${escaped_html}
   </content>
 </entry>
 EOF
-    done < "$tsvdb"
+    done < "$1"
 
     echo '</feed>'
 }
 
 # TODO add <priority> if needed
+# $1 = categories
 generate_sitemap() {
-    categories="$1"
+    last_published="$(git_timestamps_iso8601 published | head -1)"
 
     cat <<EOF
 <?xml version='1.0' encoding='UTF-8'?>
@@ -146,18 +153,19 @@ generate_sitemap() {
     xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 <url>
   <loc>https://${DOMAIN}/</loc>
-  <lastmod>$(git_timestamps_iso8601 published | head -1)</lastmod>
+  <lastmod>${last_published}</lastmod>
   <changefreq>weekly</changefreq>
 </url>
 EOF
 
-    for category in $categories; do
-        find "build/$category" -type f -name '*.html' | while IFS= read -r html; do
+    for category in $1; do
+        find "build/${category}" -type f -name '*.html' | while IFS= read -r html; do
             gmipath="${html%.html}.gmi"
+            lastmod="$(git_timestamps_iso8601 "published/${gmipath#build/}" | head -1)"
             cat <<EOF
 <url>
   <loc>https://${DOMAIN}/${html#build/}</loc>
-  <lastmod>$(git_timestamps_iso8601 "published/${gmipath#build/}" | head -1)</lastmod>
+  <lastmod>${lastmod}</lastmod>
   <changefreq>weekly</changefreq>
 </url>
 EOF
@@ -167,8 +175,10 @@ EOF
     echo "</urlset>"
 }
 
+# $1 = tsv DB
 generate_front_page() {
-    tsvdb="$1"
+    posts_feed_entries="$(gmi_feed_entries "$1" "posts")"
+    recipe_feed_entries="$(gmi_feed_entries "$1" "recipes")"
 
     cat <<EOF
 # Siva Mahadevan
@@ -187,11 +197,11 @@ Here are some things I've written:
 
 ### Blog Posts
 
-$(gmi_feed_entries "$tsvdb" "posts")
+${posts_feed_entries}
 
 ### Vegetarian Recipes
 
-$(gmi_feed_entries "$tsvdb" "recipes")
+${recipe_feed_entries}
 
 ## Contact
 
@@ -204,20 +214,18 @@ EOF
 
 }
 
+# $1 = pubtype
 package() {
-    pubtype="$1"
-    pubdest="package/$pubtype"
-
     find assets -type f | while IFS= read -r asset; do
-        assetpath="$pubdest/${asset#assets/}"
-        dirname "$assetpath" | xargs mkdir -p
-        cp "$asset" "$assetpath"
+        assetpath="package/$1/${asset#assets/}"
+        dirname "${assetpath}" | xargs mkdir -p
+        cp "${asset}" "${assetpath}"
     done
 
-    find build -type f -name "*.$pubtype" | while IFS= read -r f; do
-        destpath="$pubdest/${f#build/}"
-        dirname "$destpath" | xargs mkdir -p
-        cp "$f" "$destpath"
+    find build -type f -name "*.$1" | while IFS= read -r f; do
+        destpath="package/$1/${f#build/}"
+        dirname "${destpath}" | xargs mkdir -p
+        cp "${f}" "${destpath}"
     done
 }
 
@@ -229,6 +237,6 @@ case "$1" in
     generate_sitemap) generate_sitemap "$2" ;;
     package) package "$2" ;;
     *)
-        echo "Unknown command: '$1'" >&2
-        exit 1
+        echo "$0: ERROR: Unknown command: '$1'" >&2
+        exit 64 # EX_USAGE
 esac
