0299c715d257bafdbe986093bf52c3c580db6499
Siva Mahadevan <me@svmhdvn.name>
2024-09-10 22:22:28 -0400

    WIP refactor for common functionality

---
+++ sites/svmhdvn.name/blog.sh
@@ -0,0 +1,97 @@
+#!/bin/sh
+set -eu
+
+. ../../util.sh
+
+# $1 = tsvdb
+generate_atom_feed() {
+    last_updated="$(git_timestamps_iso8601 . | head -1)"
+    cat <<EOF
+<?xml version="1.0" encoding="utf-8"?>
+<feed xmlns="http://www.w3.org/2005/Atom">
+  <title>${FULLNAME}</title>
+  <id>tag:${DOMAIN},2024-02-01:default-atom-feed</id>
+  <link href="https://${DOMAIN}/" />
+  <link href="https://${DOMAIN}/feed.xml" rel="self" type="application/atom+xml" />
+  <updated>${last_updated}</updated>
+  <generator uri="https://git.sr.ht/~${USERNAME}/blog/tree/main/item/blog.sh">blog.sh</generator>
+  <icon>https://${DOMAIN}/favicon.ico</icon>
+  <logo>https://${DOMAIN}/siva.jpg</logo>
+  <subtitle>${FULLNAME}'s Blog</subtitle>
+  <author>
+    <name>${FULLNAME}</name>
+    <uri>https://${DOMAIN}/</uri>
+    <email>${EMAIL}</email>
+  </author>
+EOF
+
+    while IFS='	' read -r created updated title gmi; do
+        day=$(echo "${created}" | iso8601_date_only)
+        escaped_html="$(escape_html < "build/${gmi%.gmi}.article")"
+        cat <<EOF
+<entry>
+  <title>${title}</title>
+  <id>tag:${DOMAIN},${day}:${gmi%.gmi}.html</id>
+  <link rel="alternate" href="https://${DOMAIN}/${gmi%.gmi}.html"/>
+  <published>${created}</published>
+  <updated>${updated}</updated>
+  <content type="html">
+${escaped_html}
+  </content>
+</entry>
+EOF
+    done < "$1"
+
+    echo '</feed>'
+}
+
+# $1 = tsv DB
+generate_front_page() {
+    posts_feed_entries="$(gmi_feed_entries "$1" posts)"
+    recipe_feed_entries="$(gmi_feed_entries "$1" recipes)"
+
+    cat <<EOF
+# Siva Mahadevan
+
+Hey :) Welcome to my blog!
+
+## Directory
+
+=> /me/ About Me
+=> /posts/ Blog Posts
+=> /recipes/ Vegetarian Recipes
+
+## Feed
+
+Here are some things I've written:
+
+### Blog Posts
+
+${posts_feed_entries}
+
+### Vegetarian Recipes
+
+${recipe_feed_entries}
+
+## Contact
+
+I'd love to hear your comments on my posts! You can comment publically by emailing my public inbox or privately at my personal email:
+
+=> mailto:~${USERNAME}/public-inbox@lists.sr.ht Write a comment
+=> https://lists.sr.ht/~${USERNAME}/public-inbox Public inbox archives
+=> mailto:${EMAIL} Email me
+EOF
+
+}
+
+case "$1" in
+    index_tsv) index_tsv ;;
+    article_to_html) article_to_html "$2" "$3" ;;
+    generate_atom_feed) generate_atom_feed "$2" ;;
+    generate_front_page) generate_front_page "$2" ;;
+    generate_sitemap) generate_sitemap "$2" ;;
+    package) package "$2" ;;
+    *)
+        echo "$0: ERROR: Unknown command: '$1'" >&2
+        exit 64 # EX_USAGE
+esac
--- util.sh
+++ util.sh
@@ -30,3 +30,89 @@ package() {
         cp "${f}" "${destpath}"
     done
 }
+
+gmi_title() {
+    sed -n '/^# /{s/# //p; q}'
+}
+
+# Build Tab-Separated Values (TSV) file containing all the posts (excluding
+# front page content)
+index_tsv() {
+    find published -type f -name '*.gmi' ! -name 'index.gmi' | while IFS= read -r gmi; do
+        git_history=$(git_timestamps_iso8601 "${gmi}")
+        created="$(echo "${git_history}" | tail -1)"
+        updated="$(echo "${git_history}" | head -1)"
+        gmi_title="$(sed -n '/^# /{s/# //p; q}' "${gmi}")"
+        printf '%s\t%s\t%s\t%s\n' \
+            "${created}" \
+            "${updated}" \
+            "${gmi_title}" \
+            "${gmi#published/}"
+    done | sort -r
+}
+
+# $1 = tsvdb
+# $2 = category
+gmi_feed_entries() {
+    grep "$2/" "$1" | while IFS='	' read -r created updated title f; do
+        created_date="$(echo "${created}" | iso8601_date_only)"
+        echo "=> /${f} ${created_date} - ${title}"
+    done
+}
+
+# $1 = article
+# $2 = nav_categories
+article_to_html() {
+    built_gmi="${1%.article}.gmi"
+    published_gmi="published/${built_gmi#build/}"
+
+    # shellcheck disable=SC2310
+    last_updated_history="$(git_timestamps_human "${published_gmi}" || git_timestamps_human published)"
+    last_updated="$(echo "${last_updated_history}" | head -1)"
+
+    blog_title="$(gmi_title < "${built_gmi}")"
+    sed \
+        -e "s|%%SITE_TITLE%%|${site_title}|g" \
+        -e "s|%%NAV_TITLE%%|${NAV_TITLE}|g" \
+        -e "s|%%NAV_CATEGORIES%%|$2|g" \
+        templates/header.html.in
+    cat "$1"
+    sed \
+        -e "s|%%LAST_UPDATED%%|${last_updated}|g" \
+        -e "s|%%GMI_URL%%|gemini://${DOMAIN}/${built_gmi#build/}|g" \
+        templates/footer.html.in
+}
+
+# TODO add <priority> if needed
+# $1 = categories
+generate_sitemap() {
+    last_published="$(git_timestamps_iso8601 published | head -1)"
+
+    cat <<EOF
+<?xml version='1.0' encoding='UTF-8'?>
+<urlset xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+    xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd"
+    xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+<url>
+  <loc>https://${DOMAIN}/</loc>
+  <lastmod>${last_published}</lastmod>
+  <changefreq>weekly</changefreq>
+</url>
+EOF
+
+    for category in $1; do
+        find "build/${category}" -type f -name '*.html' | while IFS= read -r html; do
+            gmipath="${html%.html}.gmi"
+            lastmod="$(git_timestamps_iso8601 "published/${gmipath#build/}" | head -1)"
+            cat <<EOF
+<url>
+  <loc>https://${DOMAIN}/${html#build/}</loc>
+  <lastmod>${lastmod}</lastmod>
+  <changefreq>weekly</changefreq>
+</url>
+EOF
+        done
+    done
+
+    echo "</urlset>"
+}
